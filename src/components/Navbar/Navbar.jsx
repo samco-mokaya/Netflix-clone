c3f55578271ef8db4859a4a434fec256cddae494
samco-mokaya <gmansesco@gmail.com>
2025-06-18 19:04:53 +0300

    Initial commit ;set up the components/pages/assets

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import './Navbar.css'
+import logo from '../../assets/logo.png' 
+import search_icon from '../../assets/search_icon.svg'
+import caret from '../../assets/caret_icon.svg'
+import bell_icon from '../../assets/bell_icon.svg'
+const Navbar = () => {
+  return (
+    <div className='navbar'>
+      <div className="navbar-left">
+        <img src={logo} alt="VisioFlix Logo" />
+        <ul>
+          <li>Home</li>
+          <li>TV shows</li>
+          <li>Movies</li>
+          <li>New and Popular</li>
+          <li>My List</li>
+          <li>Browse by Languages</li>
+        </ul>
+      </div>
+      <div className="navbar-right">
+        <img src={search_icon} alt="" />
+        <p>Children </p>
+        <img src={bell_icon} alt="" />
+        <div className="navbar profile">
+          
+        </div>
+      </div>
+      </div>
+      
+   
+  )
+}
+
+export default Navbar
---
+++ src/components/TitleCards/TitleCards.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './TitleCards.css'
+const TitleCards = () => {
+  return (
+    <div className='title-cards'> 
+      
+    </div>
+  )
+}
+
+export default TitleCards
