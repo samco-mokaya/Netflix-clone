8f57e89e19bd51462147415518c000ac2cd3883d
samco-mokaya <gmansesco@gmail.com>
2025-06-19 07:55:18 +0300

    Finalized on  the Navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,30 +2,35 @@ import React from 'react'
 import './Navbar.css'
 import logo from '../../assets/logo.png' 
 import search_icon from '../../assets/search_icon.svg'
-import caret from '../../assets/caret_icon.svg'
+import caret_icon from '../../assets/caret_icon.svg'
 import profile_img from '../../assets/profile_img.png'
 import bell_icon from '../../assets/bell_icon.svg'
 const Navbar = () => {
   return (
     <div className='navbar'>
       <div className="navbar-left">
-        <img src={logo} alt="VisioFlix Logo" />
+        <img src={logo} alt="" className='' />
         <ul>
           <li>Home</li>
           <li>TV shows</li>
           <li>Movies</li>
           <li>New and Popular</li>
           <li>My List</li>
-          <li>Browse by Languages</li>
+          <li>Browse by language</li>
         </ul>
       </div>
+
       <div className="navbar-right">
-        <img src={search_icon} alt="" />
-        <p>Children </p>
-        <img src={bell_icon} alt="" />
-        <div className="navbar profile">
+        <img src={search_icon} alt="" className='icons' />
+             <p>Children</p>
+          <img src={bell_icon} alt="" className='icons' />
+     
+        <div className="navbar-profile"> 
           <img src={profile_img} alt="" className='profile' />
-          <img src={caret} alt="" />
+          <img src={caret_icon} alt="" />
+          <div className="dropdown">
+            <p>Sign Out of Netflix</p>
+          </div>
         </div>
       </div>
       </div>
